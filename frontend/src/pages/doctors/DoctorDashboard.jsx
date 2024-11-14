c50904ba9ebf24fe9cec119678eda54b0950d4b2
Sankalpa <joshisankalpa2@gmail.com>
2024-11-14 18:18:21 +0545

    dashboard ui improve and some file addeed

--- frontend/src/pages/doctors/DoctorDashboard.jsx
+++ frontend/src/pages/doctors/DoctorDashboard.jsx
@@ -18,10 +18,10 @@ const DoctorDashboard = () => {
 
           {/* Calendar Section */}
           <div className='flex space-x-4 overflow-x-auto m-auto justify-center'>
-            {["21 Sep", "22 Sep", "23 Sep", "24 Sep", "25 Sep", "26 Sep", "27 Sep", "28 Sep"].map((date, index) => {
+            {["21 nov", "22 nov", "23 nov", "24 nov", "25 nov", "26 nov", "27 nov"].map((date, index) => {
               const [day, month] = date.split(" ");
               return (
-                <div key={index} className='p-6 bg-white rounded-lg shadow-md text-center flex flex-col justify-center items-center'>
+                <div key={index} className='p-6 bg-white rounded-lg  text-center flex flex-col justify-center items-center border-2 border-gray-400'>
                   <span className="text-gray-500 font-bold text-2xl">{day}</span>
                   <span className="text-gray-500 font-semibold">{month}</span>
                 </div>
@@ -33,20 +33,20 @@ const DoctorDashboard = () => {
 
           {/* Dashboard Data Section */}
           <div className='grid grid-cols-4 gap-4'>
-            <div className="text-center bg-orange-100 p-4 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold text-teal-700">10</h3>
+            <div className="text-center border-2 border-orange-200 bg-orange-100 p-4 rounded-lg shadow-md h-32 flex flex-col justify-center items-center">
+              <h3 className="text-5xl font-bold text-gray-600">10</h3>
               <p className="text-gray-600">Appointments</p>
             </div>
-            <div className="text-center bg-orange-100 p-4 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold text-teal-700">100</h3>
+            <div className="text-center  border-2 border-orange-200 bg-orange-100 p-4 rounded-lg shadow-md flex flex-col justify-center items-center">
+              <h3 className="text-5xl font-bold text-gray-600">100</h3>
               <p className="text-gray-600">Patients Checked</p>
             </div>
-            <div className="text-center bg-orange-100 p-4 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold text-teal-700">5</h3>
+            <div className="text-center border-2 border-orange-200 bg-orange-100 p-4 rounded-lg shadow-md flex flex-col justify-center items-center">
+              <h3 className="text-5xl font-bold text-gray-600">5</h3>
               <p className="text-gray-600">New Messages</p>
             </div>
-            <div className="text-center bg-orange-100 p-4 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold text-teal-700">12</h3>
+            <div className="text-center border-2 border-orange-200 bg-orange-100 p-4 rounded-lg shadow-md flex flex-col justify-center items-center">
+              <h3 className="text-5xl font-bold text-gray-600">12</h3>
               <p className="text-gray-600">Tasks Completed</p>
             </div>
           </div>
@@ -54,10 +54,10 @@ const DoctorDashboard = () => {
 
           {/* Buttons Section */}
           <div className='flex space-x-4'>
-            <button className='flex-1 py-2 bg-teal-500 text-white rounded-lg shadow-md hover:bg-teal-600'>
+            <button className='flex-1 py-3 border-2 border-teal-600 text-gray-600 font-semibold rounded-lg hover:bg-teal-600'>
               View Appointments
             </button>
-            <button className='flex-1 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600'>
+            <button className='flex-1 py-2 border-2 border-teal-600 text-gray-600 font-semibold rounded-lg hover:bg-teal-600'>
               View Messages
             </button>
           </div>
